76945c1db51f6aca061f72ff0b71ac7327e6077a
Gaston Ferreyra <ferreyragaston351@gmail.com>
2022-05-28 00:29:04 +0100

    Inicio

---
+++ Solucion/Video Club/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Video_Club
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BarraTitulo = new System.Windows.Forms.Panel();
+            this.PicMin = new System.Windows.Forms.PictureBox();
+            this.PicMax = new System.Windows.Forms.PictureBox();
+            this.PicSalir = new System.Windows.Forms.PictureBox();
+            this.MenuVertical = new System.Windows.Forms.Panel();
+            this.PanelSalir = new System.Windows.Forms.Panel();
+            this.PicRestaurar = new System.Windows.Forms.PictureBox();
+            this.BarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSalir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicRestaurar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarraTitulo
+            // 
+            this.BarraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(10)))), ((int)(((byte)(87)))));
+            this.BarraTitulo.Controls.Add(this.PicRestaurar);
+            this.BarraTitulo.Controls.Add(this.PicMin);
+            this.BarraTitulo.Controls.Add(this.PicMax);
+            this.BarraTitulo.Controls.Add(this.PicSalir);
+            this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraTitulo.Name = "BarraTitulo";
+            this.BarraTitulo.Size = new System.Drawing.Size(1300, 35);
+            this.BarraTitulo.TabIndex = 1;
+            // 
+            // PicMin
+            // 
+            this.PicMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicMin.Image = ((System.Drawing.Image)(resources.GetObject("PicMin.Image")));
+            this.PicMin.Location = new System.Drawing.Point(1201, 7);
+            this.PicMin.Name = "PicMin";
+            this.PicMin.Size = new System.Drawing.Size(25, 25);
+            this.PicMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicMin.TabIndex = 0;
+            this.PicMin.TabStop = false;
+            this.PicMin.Click += new System.EventHandler(this.PicMin_Click);
+            // 
+            // PicMax
+            // 
+            this.PicMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicMax.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicMax.Image = ((System.Drawing.Image)(resources.GetObject("PicMax.Image")));
+            this.PicMax.Location = new System.Drawing.Point(1232, 7);
+            this.PicMax.Name = "PicMax";
+            this.PicMax.Size = new System.Drawing.Size(25, 25);
+            this.PicMax.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicMax.TabIndex = 0;
+            this.PicMax.TabStop = false;
+            this.PicMax.Click += new System.EventHandler(this.PicMax_Click);
+            // 
+            // PicSalir
+            // 
+            this.PicSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicSalir.Image = ((System.Drawing.Image)(resources.GetObject("PicSalir.Image")));
+            this.PicSalir.Location = new System.Drawing.Point(1263, 7);
+            this.PicSalir.Name = "PicSalir";
+            this.PicSalir.Size = new System.Drawing.Size(25, 25);
+            this.PicSalir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicSalir.TabIndex = 0;
+            this.PicSalir.TabStop = false;
+            this.PicSalir.Click += new System.EventHandler(this.PicSalir_Click);
+            // 
+            // MenuVertical
+            // 
+            this.MenuVertical.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(10)))), ((int)(((byte)(60)))));
+            this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuVertical.Location = new System.Drawing.Point(0, 35);
+            this.MenuVertical.Name = "MenuVertical";
+            this.MenuVertical.Size = new System.Drawing.Size(220, 615);
+            this.MenuVertical.TabIndex = 2;
+            // 
+            // PanelSalir
+            // 
+            this.PanelSalir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelSalir.Location = new System.Drawing.Point(220, 35);
+            this.PanelSalir.Name = "PanelSalir";
+            this.PanelSalir.Size = new System.Drawing.Size(1080, 615);
+            this.PanelSalir.TabIndex = 3;
+            // 
+            // PicRestaurar
+            // 
+            this.PicRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicRestaurar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicRestaurar.Image = ((System.Drawing.Image)(resources.GetObject("PicRestaurar.Image")));
+            this.PicRestaurar.Location = new System.Drawing.Point(1232, 7);
+            this.PicRestaurar.Name = "PicRestaurar";
+            this.PicRestaurar.Size = new System.Drawing.Size(25, 25);
+            this.PicRestaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicRestaurar.TabIndex = 0;
+            this.PicRestaurar.TabStop = false;
+            this.PicRestaurar.Click += new System.EventHandler(this.PicRestaurar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(2)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1300, 650);
+            this.Controls.Add(this.PanelSalir);
+            this.Controls.Add(this.MenuVertical);
+            this.Controls.Add(this.BarraTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.BarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PicMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSalir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicRestaurar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BarraTitulo;
+        private System.Windows.Forms.Panel MenuVertical;
+        private System.Windows.Forms.PictureBox PicMin;
+        private System.Windows.Forms.PictureBox PicMax;
+        private System.Windows.Forms.PictureBox PicSalir;
+        private System.Windows.Forms.Panel PanelSalir;
+        private System.Windows.Forms.PictureBox PicRestaurar;
+    }
+}
+
---
+++ Solucion/Video Club/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.FileLoadException;
+
+namespace Video_Club
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PicSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PicMax_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            PicMax.Visible = false;
+            PicRestaurar.Visible = true;
+        }
+
+        private void PicMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            
+        }
+
+        private void PicRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            PicMax.Visible = true;
+            PicRestaurar.Visible = false;
+        }
+    }
+}
