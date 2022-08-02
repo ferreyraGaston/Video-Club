2491b1cfdf64845e49a06c6839569d0094699c62
gastonloco <ferreyragaston351@gmail.com>
2022-08-02 15:48:24 -0300

    constructor entidades

--- Solucion/Entidades/Producto.cs
+++ Solucion/Entidades/Producto.cs
@@ -8,5 +8,33 @@ namespace Entidades
 {
     internal class Producto
     {
+
+        /***************DEFINO LOS ATRIBUTOS*******************/
+
+        //agrego el modificador de acceso private para resetar el 
+        //encapsulamiento de los objetos
+        private int codigo;
+        private string descripcion;
+        private int stock;
+        private double precio;
+
+        /**********DEFINO EL CONSTRUCTOR POR DEFECTO**********/
+        // por lo general son publicos y llevan el nombre de la clase
+
+        public Producto()
+        {
+        }
+
+        /**********DEFINO EL CONSTRUCTOR**********/
+        // los contructor sirve para inicializar los objetos
+
+        public Producto(int codigo, string descripcion, int precio)
+        {
+            this.codigo = codigo;          
+            this.descripcion = descripcion;
+            this.precio = precio;   
+
+        }
+
     }
 }
